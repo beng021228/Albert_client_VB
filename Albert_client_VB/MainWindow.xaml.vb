d74b72ab5b0814a938f9aa66ecccc1b50a504e42
beng021228 <35044916+beng021228@users.noreply.github.com>
2020-09-30 12:40:28 -0400

    Add files via upload

---
+++ Albert_client_VB/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Class MainWindow
+
+    'Important!!!
+
+
+    Private Sub buttonRtsp_Click(sender As Object, e As RoutedEventArgs)
+
+
+    End Sub           'Set the vlc plugins directory path
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+
+
+    End Sub
+
+    Private Sub MainWindowOnClosing(sender As Object, e As RoutedEventArgs)
+
+
+
+    End Sub
+
+    Private Sub MediaOnParsedChanged(sender As Vlc.DotNet.Core.VlcMedia, e As Vlc.DotNet.Core.VlcMediaFreedEventArgs)
+
+
+    End Sub
+
+
+End Class
