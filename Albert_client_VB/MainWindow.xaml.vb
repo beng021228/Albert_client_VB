848c36ae52a1ecb081ceec5a94233adec8221cde
Benjamin Greyerbiehl <greyerbiehlbt@outlook.com>
2020-09-30 15:34:08 -0400

    Updated

--- Albert_client_VB/MainWindow.xaml.vb
+++ Albert_client_VB/MainWindow.xaml.vb
@@ -25,5 +25,27 @@
 
     End Sub
 
+    Private Sub Dirfwd_Click(sender As Object, e As RoutedEventArgs) Handles dirfwd.Click
+        textblock_1.Text += vbCrLf & "fwd button clicked"
+    End Sub
 
+
+
+
+
+    Private Sub Dirfwd_PreviewMouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles dirfwd.PreviewMouseLeftButtonDown
+        textblock_1.Text += vbCrLf & "driving forward"
+    End Sub
+
+    Private Sub Dirfwd_PreviewMouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles dirfwd.PreviewMouseLeftButtonUp
+        textblock_1.Text += vbCrLf & "stopping"
+    End Sub
+
+    Private Sub Dirleft_PreviewMouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles dirleft.PreviewMouseLeftButtonDown
+        textblock_1.Text += vbCrLf & "turning left"
+    End Sub
+
+    Private Sub Dirleft_PreviewMouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles dirleft.PreviewMouseLeftButtonUp
+        textblock_1.Text += vbCrLf & "stopping"
+    End Sub
 End Class
